4e8eec774c436c7719951e4dd243bfa1373124c8
Levin Baenninger <levin.baenninger@buhlergroup.com>
2023-10-27 15:30:14 +0200

    Add new features (V1.1)

--- Exercises/Teams/Exercise_4/exercise_4.c
+++ Exercises/Teams/Exercise_4/exercise_4.c
@@ -2,45 +2,45 @@
     Name: exercise_4.c
     Author: Levin Baenninger
     Date: 27.10.2023
-    Version: 1.0
+    Version: 1.1
 */
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+#include <ctype.h>
 
 int main()
 {
     // Number guessing game
     // Declaring Variables
-    int randnum, guess, count;
+    int randnum, guess, count, difficulty;
 
-    // Calculate random number
-    randnum = rand() % 1000 + 1;
+    // TODO: Type checker
 
     // Get user input
-    printf("Welcome to the number guessing game! Please type in your first guess: ");
+    printf("Welcome to the number guessing game! Which number should be the maximum? ");
+    scanf("%d", &difficulty);
+
+    // Calculate random number
+    srand(time(NULL));
+    randnum = rand() % difficulty + 1;
+
+    // Get user input
+    printf("\nPlease type in your first guess: ");
     scanf("%d", &guess);
     count++;
 
     while (guess != randnum)
     {
         if (guess < randnum)
-        {
             printf("\nRandom number is bigger!");
-        }
         else if (guess > randnum)
-        {
             printf("\nRandom number is smaller!");
-        }
-        else
-        {
-            printf("\nNot a valid number!");
-            count--;
-        }
 
-        count++;
-        printf("\nPlease type in your next guess: ");
+        printf("\nPlease type in your guess: ");
         scanf("%d", &guess);
+        count++;
     }
 
     printf("\nYou have found the random number! It took you %d guesses.", count);
